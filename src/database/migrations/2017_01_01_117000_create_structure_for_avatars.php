38f12579a9e4f929df2d31429d696b0975aa0cd9
mishu <mishu@xtelecom.ro>
2017-06-23 14:08:14 +0300

    updated structure migration

--- src/database/migrations/2017_01_01_117000_create_structure_for_avatars.php
+++ src/database/migrations/2017_01_01_117000_create_structure_for_avatars.php
@@ -5,12 +5,12 @@ use LaravelEnso\Core\app\Classes\StructureManager\StructureMigration;
 class CreateStructureForAvatars extends StructureMigration
 {
     protected $permissionGroup = [
-        'name' => 'core.avatars', 'description' => 'Avatars Permissions Group',
+        'name' => 'core.avatars', 'description' => 'Avatars permissions group',
     ];
 
     protected $permissions = [
-        ['name' => 'core.avatars.destroy', 'description' => 'Delete Avatar', 'type' => 1, 'default' => true],
-        ['name' => 'core.avatars.show', 'description' => 'Return Selected Avatar', 'type' => 0, 'default' => true],
-        ['name' => 'core.avatars.store', 'description' => 'Upload Avatar', 'type' => 1, 'default' => true],
+        ['name' => 'core.avatars.destroy', 'description' => 'Delete avatar', 'type' => 1, 'default' => true],
+        ['name' => 'core.avatars.show', 'description' => 'Display selected avatar', 'type' => 0, 'default' => true],
+        ['name' => 'core.avatars.store', 'description' => 'Upload avatar', 'type' => 1, 'default' => true],
     ];
 }
