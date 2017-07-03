c9dad64eadecdb5b8d34a365b777551335f76627
aocneanu <aocneanu@gmail.com>
2017-07-03 11:35:10 +0000

    Apply fixes from StyleCI

--- src/AvatarServiceProvider.php
+++ src/AvatarServiceProvider.php
@@ -8,12 +8,12 @@ class AvatarServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
 
         $this->publishes([
-            __DIR__ . '/storage/app' => storage_path('app'),
-            __DIR__ . '/storage/app/avatars/profile.png' => storage_path('app/avatars/profile.png'),
+            __DIR__.'/storage/app'                     => storage_path('app'),
+            __DIR__.'/storage/app/avatars/profile.png' => storage_path('app/avatars/profile.png'),
         ], 'avatars-storage');
     }
 
