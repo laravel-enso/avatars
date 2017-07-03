db1079dd9d0b8d5b39a6bb851ab6d400faf946da
mishu <mishu@xtelecom.ro>
2017-07-03 14:35:02 +0300

    fix service provider

--- src/AvatarServiceProvider.php
+++ src/AvatarServiceProvider.php
@@ -3,23 +3,22 @@
 namespace LaravelEnso\AvatarManager;
 
 use Illuminate\Support\ServiceProvider;
-use LaravelEnso\ImageTransformer\ImageTransformerServiceProvider;
 
 class AvatarServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 
         $this->publishes([
-            __DIR__.'/storage/app'                     => storage_path('app'),
-            __DIR__.'/storage/app/avatars/profile.png' => storage_path('app/avatars/profile.png'),
+            __DIR__ . '/storage/app' => storage_path('app'),
+            __DIR__ . '/storage/app/avatars/profile.png' => storage_path('app/avatars/profile.png'),
         ], 'avatars-storage');
     }
 
     public function register()
     {
-        $this->app->register(ImageTransformerServiceProvider::class);
+        //
     }
 }
