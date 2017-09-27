24308cd4cd01913d52f507edb274b08759e957f6
mishu <mishu@xtelecom.ro>
2017-09-27 11:53:38 +0300

    fix tests

--- tests/features/AvatarTest.php
+++ tests/features/AvatarTest.php
@@ -5,17 +5,18 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use LaravelEnso\AvatarManager\app\Models\Avatar;
-use LaravelEnso\TestHelper\app\Classes\TestHelper;
+use LaravelEnso\TestHelper\app\Traits\SignIn;
+use Tests\TestCase;
 
-class AvatarTest extends TestHelper
+class AvatarTest extends TestCase
 {
-    use DatabaseMigrations;
+    use DatabaseMigrations, SignIn;
 
     protected function setUp()
     {
         parent::setUp();
 
-        // $this->disableExceptionHandling();
+        // $this->withoutExceptionHandling();
         config()->set('enso.config.paths.avatars', 'testFolder');
         $this->signIn(User::first());
     }
@@ -26,7 +27,7 @@ class AvatarTest extends TestHelper
         $this->uploadAvatar();
         $avatar = Avatar::first();
 
-        $this->get('/core/avatars/'.$avatar->id)
+        $this->get('/core/avatars/' . $avatar->id)
             ->assertStatus(200);
 
         $this->cleanUp();
@@ -39,7 +40,7 @@ class AvatarTest extends TestHelper
         $avatar = Avatar::first();
 
         $this->assertNotNull($avatar);
-        Storage::assertExists('testFolder/'.$avatar->saved_name);
+        Storage::assertExists('testFolder/' . $avatar->saved_name);
 
         $this->cleanUp();
     }
@@ -50,9 +51,9 @@ class AvatarTest extends TestHelper
         $this->uploadAvatar();
         $avatar = Avatar::first();
 
-        $this->delete('/core/avatars/'.$avatar->id);
+        $this->delete('/core/avatars/' . $avatar->id);
 
-        Storage::assertMissing('testFolder/'.$avatar->saved_name);
+        Storage::assertMissing('testFolder/' . $avatar->saved_name);
         $this->assertNull($avatar->fresh());
 
         $this->cleanUp();
@@ -61,7 +62,7 @@ class AvatarTest extends TestHelper
     private function uploadAvatar()
     {
         $this->post('/core/avatars/', [
-                'file' => UploadedFile::fake()->image('avatar.png'),
+            'file' => UploadedFile::fake()->image('avatar.png'),
         ]);
     }
 
