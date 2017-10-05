2eba6a31b581d637d8327e722605bab1caba9270
mishu <mishu@xtelecom.ro>
2017-10-05 14:51:07 +0300

    refactor routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-Route::namespace('LaravelEnso\AvatarManager\app\Http\Controllers')
-    ->prefix('api')
+Route::middleware(['web', 'auth', 'core'])
+    ->namespace('LaravelEnso\AvatarManager\app\Http\Controllers')
+    ->prefix('api/core')->as('core.')
     ->group(function () {
-        Route::prefix('core')->as('core.')
-            ->group(function () {
-                Route::resource('avatars', 'AvatarController', ['only' => ['show', 'store', 'destroy']]);
-            });
+        Route::resource('avatars', 'AvatarController', ['only' => ['show', 'store', 'destroy']]);
     });
