5f66b163b7828d03163a56b75163593bc265fd84
Adrian Ocneanu <aocneanu@gmail.com>
2018-01-18 06:17:25 +0000

    Apply fixes from StyleCI

--- src/app/Http/Controllers/AvatarController.php
+++ src/app/Http/Controllers/AvatarController.php
@@ -5,8 +5,8 @@ namespace LaravelEnso\AvatarManager\app\Http\Controllers;
 use App\Http\Controllers\Controller;
 use LaravelEnso\AvatarManager\app\Models\Avatar;
 use LaravelEnso\AvatarManager\app\Classes\Storer;
-use LaravelEnso\AvatarManager\app\Classes\Presenter;
 use LaravelEnso\AvatarManager\app\Classes\Destroyer;
+use LaravelEnso\AvatarManager\app\Classes\Presenter;
 use LaravelEnso\AvatarManager\app\Http\Requests\ValidateAvatarRequest;
 
 class AvatarController extends Controller
