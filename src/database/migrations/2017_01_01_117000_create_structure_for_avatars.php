b31f91b4770c392bbf31e4c2cbe4c5b9a7e3d35c
mishu <mishu@xtelecom.ro>
2018-05-28 13:19:55 +0300

    updates for the new structure manager

--- src/database/migrations/2017_01_01_117000_create_structure_for_avatars.php
+++ src/database/migrations/2017_01_01_117000_create_structure_for_avatars.php
@@ -9,8 +9,8 @@ class CreateStructureForAvatars extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'core.avatars.destroy', 'description' => 'Delete avatar', 'type' => 1, 'default' => true],
-        ['name' => 'core.avatars.show', 'description' => 'Display selected avatar', 'type' => 0, 'default' => true],
-        ['name' => 'core.avatars.store', 'description' => 'Upload avatar', 'type' => 1, 'default' => true],
+        ['name' => 'core.avatars.destroy', 'description' => 'Delete avatar', 'type' => 1, 'is_default' => true],
+        ['name' => 'core.avatars.show', 'description' => 'Display selected avatar', 'type' => 0, 'is_default' => true],
+        ['name' => 'core.avatars.store', 'description' => 'Upload avatar', 'type' => 1, 'is_default' => true],
     ];
 }
