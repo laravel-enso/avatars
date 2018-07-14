cbbb3765a1d8e025f526ef34626d8b55e8d7a5c6
mishu <mishu@xtelecom.ro>
2018-07-14 15:43:13 +0300

    adds cleanup on upload

--- src/app/Classes/Storer.php
+++ src/app/Classes/Storer.php
@@ -32,6 +32,7 @@ class Storer extends Handler
 
         try {
             \DB::transaction(function () use (&$avatar) {
+                $this->user->avatar->delete();
                 $this->processImage();
                 $this->fileManager->startUpload($this->avatar);
                 $avatar = $this->store();
