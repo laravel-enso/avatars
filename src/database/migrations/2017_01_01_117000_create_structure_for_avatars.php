51d00d1c8541af710f08f0848f01269c491ad284
mishu <mishu@xtelecom.ro>
2019-12-30 17:11:10 +0200

    - adds permission type enum in structure migration - updates composer json

--- src/database/migrations/2017_01_01_117000_create_structure_for_avatars.php
+++ src/database/migrations/2017_01_01_117000_create_structure_for_avatars.php
@@ -1,12 +1,13 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
+use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForAvatars extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.avatars.update', 'description' => 'Update avatar', 'type' => 1, 'is_default' => true],
-        ['name' => 'core.avatars.show', 'description' => 'Display selected avatar', 'type' => 0, 'is_default' => true],
-        ['name' => 'core.avatars.store', 'description' => 'Upload a new avatar', 'type' => 1, 'is_default' => true],
+        ['name' => 'core.avatars.update', 'description' => 'Update avatar', 'type' => Types::Write, 'is_default' => true],
+        ['name' => 'core.avatars.show', 'description' => 'Display selected avatar', 'type' => Types::Read, 'is_default' => true],
+        ['name' => 'core.avatars.store', 'description' => 'Upload a new avatar', 'type' => Types::Write, 'is_default' => true],
     ];
 }
