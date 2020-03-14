78816e8eb79a78829592c9fd5ba9ecfebda26559
mishu <mishu@xtelecom.ro>
2020-03-14 18:04:45 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_117000_create_structure_for_avatars.php
+++ src/database/migrations/2017_01_01_117000_create_structure_for_avatars.php
@@ -1,13 +1,12 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForAvatars extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.avatars.update', 'description' => 'Update avatar', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.avatars.show', 'description' => 'Display selected avatar', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.avatars.store', 'description' => 'Upload a new avatar', 'type' => Types::Write, 'is_default' => true],
+        ['name' => 'core.avatars.update', 'description' => 'Update avatar', 'is_default' => true],
+        ['name' => 'core.avatars.show', 'description' => 'Display selected avatar', 'is_default' => true],
+        ['name' => 'core.avatars.store', 'description' => 'Upload a new avatar', 'is_default' => true],
     ];
 }
