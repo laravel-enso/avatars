7dacb3281f379812f8c96cacf7c458a0f5ebceca
Mihai <gandesc@users.noreply.github.com>
2020-10-07 14:05:27 +0000

    Apply fixes from StyleCI

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Avatars\Http\Controllers\Show;
 use LaravelEnso\Avatars\Http\Controllers\Store;
 use LaravelEnso\Avatars\Http\Controllers\Update;
-use LaravelEnso\Avatars\Http\Controllers\Show;
 
 Route::middleware(['api', 'auth', 'core'])
     ->prefix('api/core/avatars')
