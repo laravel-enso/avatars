bb9f19c9dcaa71e1e21821d65126066637c3bda4
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 13:08:30 +0200

    updated migrations

--- database/migrations/2017_01_01_117000_create_structure_for_avatars.php
+++ database/migrations/2017_01_01_117000_create_structure_for_avatars.php
@@ -2,11 +2,11 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-class CreateStructureForAvatars extends Migration
+return new class extends Migration
 {
     protected array $permissions = [
         ['name' => 'core.avatars.update', 'description' => 'Update avatar', 'is_default' => true],
         ['name' => 'core.avatars.show', 'description' => 'Display selected avatar', 'is_default' => true],
         ['name' => 'core.avatars.store', 'description' => 'Upload a new avatar', 'is_default' => true],
     ];
-}
+};
